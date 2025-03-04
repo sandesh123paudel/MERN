dce23439bc77e41b9b3c0656dcc8a2eb07ed82ff
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-04 12:20:13 +0545

    Conditional Rendering

--- REACT/18 Fragments/learning-fragment/src/App.jsx
+++ REACT/18 Fragments/learning-fragment/src/App.jsx
@@ -5,10 +5,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   let foodItems = ["Daal", "Green Vegetable", "Roti", "Milk", "Salad"];
 
+    // let foodItems = [];
+
+  // let emptyMessage=foodItems.length===0?<h3>I am still hungry</h3> :null;
+
+
+
+  // if(foodItems.length===0){
+  //   return <h3>I am still hungry</h3>
+  // }
+
   return (
     <React.Fragment>
       {/* Or <></> can also be used without any importing */}
       <h1>Healthy Foods</h1>
+      {/* {emptyMessage} */}
+      {foodItems.length===0 && <h3>I am still hungry</h3>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">{item}</li>
