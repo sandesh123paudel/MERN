ab99256509806e97ba8ef1e7642e303231c1f62d
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-04 12:07:28 +0545

    Mapping Methods in React

--- REACT/18 Fragments/learning-fragment/src/App.jsx
+++ REACT/18 Fragments/learning-fragment/src/App.jsx
@@ -3,16 +3,16 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  let foodItems = ["Daal", "Green Vegetable", "Roti", "Milk", "Salad"];
+
   return (
     <React.Fragment>
       {/* Or <></> can also be used without any importing */}
       <h1>Healthy Foods</h1>
-      <ul class="list-group">
-        <li class="list-group-item">Daal</li>
-        <li class="list-group-item">Green Vegetable</li>
-        <li class="list-group-item">Roti</li>
-        <li class="list-group-item">Milk</li>
-        <li class="list-group-item">Salad</li>
+      <ul className="list-group">
+        {foodItems.map((item) => (
+          <li key={item} className="list-group-item">{item}</li>
+        ))}
       </ul>
     </React.Fragment>
   );
