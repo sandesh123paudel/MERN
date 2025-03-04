b185348afa47ae1f44d8cca96b2b5b3e48a63713
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-04 12:50:55 +0545

    Diffrent Way of Handling Prop

--- REACT/18 Fragments/learning-fragment/src/components/Item.jsx
+++ REACT/18 Fragments/learning-fragment/src/components/Item.jsx
@@ -1,15 +1,20 @@
 const Item =(props)=>{
 
+  //// One Way to Show Data
   // let {foodItem}=props;
   // return <>
   // <li className="list-group-item">{foodItem}</li>
   // </>
 
+
+  ////Another Way
   return <>
   <li className="list-group-item">{props.foodItem}</li>
   </>
 }
 
+
+////Destructuring Way
 
 // const Item =({foodItem})=>{
 
