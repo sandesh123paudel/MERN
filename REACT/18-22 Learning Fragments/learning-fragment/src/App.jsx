b689251cdfdc20987f133e41be76341a3dcd6a3a
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-05 13:11:36 +0545

    Passing Children Completed

---
+++ REACT/18-22 Learning Fragments/learning-fragment/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FoodItems from "./components/FoodItems";
+import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
+
+function App() {
+  let foodItems = ["Daal", "Green Vegetable", "Roti", "Milk", "Salad"];
+
+  // let foodItems = [];
+
+  return (
+    <>
+      <Container>
+        <h1 className="food-heading">Healthy Foods</h1>
+        <ErrorMessage items={foodItems} />
+        <FoodItems items={foodItems} />
+      </Container>
+
+      {/* <Container>
+        <h1>
+          Multiple Container of similar properties can be used with passing
+          children
+        </h1>
+      </Container> */}
+    </>
+  );
+}
+
+export default App;
--- REACT/18-22 Learning Fragments/learning-fragment/src/components/FoodItems.jsx
+++ REACT/18-22 Learning Fragments/learning-fragment/src/components/FoodItems.jsx
@@ -1,8 +1,6 @@
 import Item from "./Item";
 
-
 const FoodItems=(props)=>{
-
   return (
     <ul className="list-group">
           {props.items.map((item) => (
