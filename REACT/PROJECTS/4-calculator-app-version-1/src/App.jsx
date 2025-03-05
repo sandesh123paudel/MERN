13b93fc5a22e39daef87d1d064c4742b48308e05
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-05 12:35:14 +0545

    Calculator UI Design Version 1 Completed

--- REACT/PROJECTS/4-calculator-app-version-1/src/App.jsx
+++ REACT/PROJECTS/4-calculator-app-version-1/src/App.jsx
@@ -1,18 +1,13 @@
 import css from "./App.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Display from "./components/Display";
+import ButtonContainer from "./components/ButtonContainer";
 
 function App() {
   return (
     <div className={css.calculator}>
-      <input type="text" className={css.display} />
-      <div className={css.buttonsContainer}>
-        <button>C</button>
-        <button>1</button>
-        <button>2</button>
-        <button>+</button>
-
-
-      </div>
+      <Display />
+      <ButtonContainer />
     </div>
   );
 }
---
+++ REACT/PROJECTS/4-calculator-app-version-1/src/components/Display.jsx
@@ -0,0 +1,6 @@
+import css from "./Display.module.css";
+const Display = () => {
+  return <input type="text" className={css.display} />;
+};
+
+export default Display;
