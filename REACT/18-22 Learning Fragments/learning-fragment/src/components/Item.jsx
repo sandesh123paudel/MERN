f2506af0a2e88f1eb2b030d871017e24ddbdd040
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-05 15:35:16 +0545

    Example Of UseState Done

--- REACT/18-22 Learning Fragments/learning-fragment/src/components/Item.jsx
+++ REACT/18-22 Learning Fragments/learning-fragment/src/components/Item.jsx
@@ -1,9 +1,9 @@
 import css from "./Item.module.css";
 
-const Item = ({ foodItem, handleBuyButton }) => {
+const Item = ({ foodItem,bought, handleBuyButton }) => {
   return (
     <>
-      <li className={`${css["kg-item"]} list-group-item`}>
+      <li className={`${css["kg-item"]} list-group-item ${bought?'active':''}`}>
         <span className={`${css["kg-span"]}`}>{foodItem}</span>
         <button
           className={`${css.button} btn btn-success`}
