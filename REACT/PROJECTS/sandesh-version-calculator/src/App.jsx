75b8fabde6cc4409245f78261df915215a56a838
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-06 23:39:10 +0545

    Sandesh Calculator UI Completed

--- REACT/PROJECTS/sandesh-version-calculator/src/App.jsx
+++ REACT/PROJECTS/sandesh-version-calculator/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let numbers = [
+    "7",
+    "8",
+    "9",
+    "DEL",
+    "4",
+    "5",
+    "6",
+    "+",
+    "1",
+    "2",
+    "3",
+    "-",
+    ".",
+    "0",
+    "/",
+    "X",
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="content">calculator</div>
+      <input
+        className="displayBox"
+        placeholder="399,981"
+        type="numeric"
+      ></input>
+      <div className="digitContainer">
+        {numbers.map((button) => (
+          <button className={button === "DEL" ? "del" : ""}>{button}</button>
+        ))}
+
+        <button className="reset">RESET</button>
+        <button className="equal">=</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p style={{ color: "white", margin: "10px" }}>
+        Made By Sandesh with ❤️❤️
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
