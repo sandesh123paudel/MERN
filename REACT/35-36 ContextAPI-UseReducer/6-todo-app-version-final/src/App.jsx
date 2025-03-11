758c9ec98795d726d1637a210eec81694fe97731
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-11 15:40:38 +0545

    App.jsx cleaned:UseReducer and Context Provider Moved to Store Folder

--- REACT/35-36 ContextAPI-UseReducer/6-todo-app-version-final/src/App.jsx
+++ REACT/35-36 ContextAPI-UseReducer/6-todo-app-version-final/src/App.jsx
@@ -2,73 +2,18 @@ import AppName from "./components/AppName";
 import AppTodo from "./components/AddTodo";
 import Message from "./components/Message";
 import TodoItems from "./components/TodoItems";
-import { useReducer } from "react";
-import { TodoItemsContext } from "./store/todo-items-store";
-
-const todoItemsReducer = (currTodoItems, action) => {
-  let newTodoItems = currTodoItems;
-  if (action.type == "NEW_ITEM") {
-    newTodoItems = [
-      ...currTodoItems,
-      { name: action.playload.item, date: action.playload.date },
-    ];
-  } else if (action.type === "DELETE_ITEM") {
-    newTodoItems = currTodoItems.filter(
-      (todo) =>
-        !(
-          todo.name === action.playload.item &&
-          todo.duedate === action.playload.date
-        )
-    );
-  }
-  return newTodoItems;
-};
+import TodoItemsContextProvider from "./store/todo-items-store";
 
 function App() {
-  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
-
-  const addNewItem = (item, date) => {
-    const newItemAction = {
-      type: "NEW_ITEM",
-      playload: {
-        item,
-        date,
-      },
-    };
-    dispatchTodoItems(newItemAction);
-  };
-
-  const deleteItem = (todoName, todoDate) => {
-    const isConfirmed = window.confirm(
-      `Do you really want to delete "${todoName}"`
-    );
-
-    if (!isConfirmed) return;
-    const deleteItemAction = {
-      type: "DELETE_ITEM",
-      playload: {
-        item: todoName,
-        date: todoDate,
-      },
-    };
-    dispatchTodoItems(deleteItemAction);
-  };
-
   return (
-    <TodoItemsContext.Provider
-      value={{
-        todoItems,
-        addNewItem,
-        deleteItem,
-      }}
-    >
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName />
         <AppTodo />
         <Message />
         <TodoItems />
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 export default App;
--- REACT/35-36 ContextAPI-UseReducer/6-todo-app-version-final/src/store/todo-items-store.jsx
+++ REACT/35-36 ContextAPI-UseReducer/6-todo-app-version-final/src/store/todo-items-store.jsx
@@ -1,7 +1,71 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const TodoItemsContext = createContext({
   todoItems: [],
   addNewItem: () => {},
   deleteItem: () => {},
 });
+
+const todoItemsReducer = (currTodoItems, action) => {
+  let newTodoItems = currTodoItems;
+  if (action.type == "NEW_ITEM") {
+    newTodoItems = [
+      ...currTodoItems,
+      { name: action.playload.item, date: action.playload.date },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currTodoItems.filter(
+      (todo) =>
+        !(
+          todo.name === action.playload.item &&
+          todo.duedate === action.playload.date
+        )
+    );
+  }
+  return newTodoItems;
+};
+
+const TodoItemsContextProvider = ({ children }) => {
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+
+  const addNewItem = (item, date) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      playload: {
+        item,
+        date,
+      },
+    };
+    dispatchTodoItems(newItemAction);
+  };
+
+  const deleteItem = (todoName, todoDate) => {
+    const isConfirmed = window.confirm(
+      `Do you really want to delete "${todoName}"`
+    );
+
+    if (!isConfirmed) return;
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      playload: {
+        item: todoName,
+        date: todoDate,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
+  };
+
+  return (
+    <TodoItemsContext.Provider
+      value={{
+        todoItems,
+        addNewItem,
+        deleteItem,
+      }}
+    >
+      {children}
+    </TodoItemsContext.Provider>
+  );
+};
+
+export default TodoItemsContextProvider;
