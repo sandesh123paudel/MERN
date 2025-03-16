7ca5de27f344cf5336d7cc572fc0a8c94124aa0e
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-16 15:02:55 +0545

    Router Added In App

--- REACT/46 React Router/7-social-media/src/main.jsx
+++ REACT/46 React Router/7-social-media/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
+import CreatePost from "./components/CreatePost.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create-post",
+    element: <CreatePost />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
