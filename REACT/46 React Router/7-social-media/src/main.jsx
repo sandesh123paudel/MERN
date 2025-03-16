8ce967459635c96759f2683f427037b744c7a6cd
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-16 15:09:29 +0545

    Layout Routing Added

--- REACT/46 React Router/7-social-media/src/main.jsx
+++ REACT/46 React Router/7-social-media/src/main.jsx
@@ -2,17 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import App from "./App.jsx";
+import App from "./routes/App.jsx";
 import CreatePost from "./components/CreatePost.jsx";
+import PostList from "./components/PostList.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/create-post",
-    element: <CreatePost />,
+    children: [
+      { path: "/", element: <PostList /> },
+      { path: "/create-post", element: <CreatePost /> },
+    ],
   },
 ]);
 
