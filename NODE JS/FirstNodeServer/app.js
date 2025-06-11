6de076bb31c3dd73c8ed70bc10fe218da534d77a
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-06-11 23:49:38 +0545

    Implement basic Node.js server setup

---
+++ NODE JS/FirstNodeServer/app.js
@@ -0,0 +1,20 @@
+//Simple Node Server
+
+// const http = require("http");
+
+// const server =http.createServer((req, res) =>{
+//   console.log(req);
+// });
+
+// server.listen(3000);
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on address: http://localhost:${PORT}`);
+});
