5e09ae473d4b0e7f9b6eba5bab89aee6ac49202d
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-06-11 23:22:59 +0545

    Add file writing functionality to output.txt

--- NODE JS/first.js
+++ NODE JS/first.js
@@ -2,5 +2,11 @@
 //   console.log("Hello");
 // }, 5000);
 
-
 console.log(global);
+
+const fs = require("fs");
+
+fs.writeFile("output.txt", " Sandesh Paude", (err) => {
+  if (err) console.log("Error");
+  else console.log("File Written");
+});
