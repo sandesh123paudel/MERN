01f167bcdc58931dba9335c9d697aa3e8a8b32e4
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-06-16 21:32:41 +0545

    Add touch and undefsafe packages with configuration files
    - Added touch package (version 3.1.1) with its package.json, including author, description, scripts, and dependencies.
    - Introduced undefsafe package (version 2.0.5) with its package.json, README.md, example.js, and library code (lib/undefsafe.js).
    - Included configuration files for undefsafe: .jscsrc, .jshintrc, .travis.yml, and GitHub Actions workflow (release.yml).
    - Added LICENSE file for undefsafe with MIT license details.
    - Updated package-lock.json to reflect the addition of new packages and their dependencies.

--- NODE JS/7-NPM & Tools/app.js
+++ NODE JS/7-NPM & Tools/app.js
@@ -2,9 +2,19 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req);
+
+  res.setHeader("Content-Body", "text/html");
+  res.write(`<html><head><title>My First Page</title></head>
+<body>
+<h1>Hello from my Node.js Server!</h1>
+<h1>Hello from my Node.js Server!</h1>
+
+</body>
+</html>`);
+  res.end();
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server running on address: http://localhost:${PORT}`);
 });
