53f96fcdfb9dc6a1792ae24ee83af372b24711b0
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-06-19 11:36:33 +0545

    Express Router

--- NODE JS/10-airbnb/app.js
+++ NODE JS/10-airbnb/app.js
@@ -1,42 +1,18 @@
 //External Module
 const express = require("express");
 
+//Local Module
+const userRouter = require("./routes/userRouter");
+const hostRouter = require("./routes/hostRouter");
+
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(req.url, req.method);
-  next();
-});
 
 app.use(express.urlencoded());
+app.use(userRouter);
+app.use(hostRouter);
 
-app.get("/", (req, res, next) => {
-  res.send(`<h1>Welcome to airbnb<h1>
-  
-    <a href="/add-home">Add Home</a>
 
-    `);
-});
-
-app.get("/add-home", (req, res, next) => {
-  res.send(`<h1>Add your Home<h1>
-    <form action="/add-home" method="POST">
-    <label for="title">Enter name of your house</label></br>
-    <input type="text" id="title" name="houseName" required>
-
-    <button type="submit">Add Home</button>
-    </form>
-
-    `);
-});
-
-app.post("/add-home", (req, res, next) => {
-  console.log(req.body.houseName);
-  res.send(`<h1>Home Registered Successfully<h1>
-     <a href="/"> Go to Home</a>
-
-    `);
-});
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ NODE JS/10-airbnb/routes/hostRouter.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const hostRouter = express.Router();
+
+hostRouter.get("/host/add-home", (req, res, next) => {
+  res.send(`<h1>Add your Home<h1>
+    <form action="/host/add-home" method="POST">
+    <label for="title">Enter name of your house</label></br>
+    <input type="text" id="title" name="houseName" required>
+
+    <button type="submit">Add Home</button>
+    </form>
+
+    `);
+});
+
+hostRouter.post("/host/add-home", (req, res, next) => {
+  console.log(req.body.houseName);
+  res.send(`<h1>Home Registered Successfully<h1>
+     <a href="/"> Go to Home</a>
+
+    `);
+});
+
+module.exports = hostRouter;
