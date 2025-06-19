7770473de232956ad1c7b46023c7ef6b71f3e9ce
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-06-19 11:44:37 +0545

    404 not found added

--- NODE JS/10-airbnb/app.js
+++ NODE JS/10-airbnb/app.js
@@ -7,12 +7,13 @@ const hostRouter = require("./routes/hostRouter");
 
 const app = express();
 
-
 app.use(express.urlencoded());
 app.use(userRouter);
 app.use(hostRouter);
 
-
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404 Page Not Found</h1>");
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
