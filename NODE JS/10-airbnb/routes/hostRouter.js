72ad847e88f4be7f15d6f2b7dd372db5ce064aee
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-06-19 11:47:28 +0545

    Common Path

--- NODE JS/10-airbnb/routes/hostRouter.js
+++ NODE JS/10-airbnb/routes/hostRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const hostRouter = express.Router();
 
-hostRouter.get("/host/add-home", (req, res, next) => {
+hostRouter.get("/add-home", (req, res, next) => {
   res.send(`<h1>Add your Home<h1>
     <form action="/host/add-home" method="POST">
     <label for="title">Enter name of your house</label></br>
@@ -13,7 +13,7 @@ hostRouter.get("/host/add-home", (req, res, next) => {
     `);
 });
 
-hostRouter.post("/host/add-home", (req, res, next) => {
+hostRouter.post("/add-home", (req, res, next) => {
   console.log(req.body.houseName);
   res.send(`<h1>Home Registered Successfully<h1>
      <a href="/"> Go to Home</a>
