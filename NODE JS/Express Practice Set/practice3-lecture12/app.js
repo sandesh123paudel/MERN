9034aa95978533661b33672c76fd020ddf057746
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-06-22 00:36:03 +0545

    Pratice Questions Added

---
+++ NODE JS/Express Practice Set/practice3-lecture12/app.js
@@ -0,0 +1,35 @@
+//Core Module
+const path = require("path");
+
+//External Module
+const express = require("express");
+
+//Local Module
+const userRouter = require("./routes/userRouter");
+const { hostRouter } = require("./routes/hostRouter");
+const rootPath = require("./utils/pathUtil");
+
+const app = express();
+
+//for ejs
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use("/host", hostRouter);
+
+//Giving access to css files
+app.use(express.static(path.join(rootPath, "public")));
+
+//Error 404 File
+app.use((req, res, next) => {
+  res.render("404", {
+    pageTitle: "!Error-Page Not Found",
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address: http://localhost:${PORT}`);
+});
